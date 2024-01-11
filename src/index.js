f7e68c5680bcdcd359daaa192beaaa06eed2a3ee
ksmith92272 <152031348+ksmith92272@users.noreply.github.com>
2024-01-10 18:24:43 -0600

    Added event listener to drop down

--- src/index.js
+++ src/index.js
@@ -12,5 +12,34 @@ function updateTime() {
 	});
 }
 
+function showTime(event) {
+	if (event.target.value.length > 0) {
+		let cityElement = document.getElementById("cities-container");
+		let citySelected = document.getElementById("cities");
+		let city = citySelected.options[citySelected.selectedIndex].text;
+
+		let timezone = event.target.value;
+		let current = moment().tz(timezone);
+		let formattedDate = current.format("MMMM Do, YYYY");
+		let formattedTime = current.format("h:mm:ss [<small>]A[</small>]");
+
+		cityElement.innerHTML = `<div
+					class="city"
+					id="${timezone}"
+				>
+					<div>
+						<h2>${city}</h2>
+						<div class="date">${formattedDate}</div>
+					</div>
+					<div>
+						<div class="time">${formattedTime}</div>
+					</div>
+				</div>`;
+	}
+}
+
+let citySelected = document.getElementById("cities");
+citySelected.addEventListener("change", showTime);
+
 updateTime();
 setInterval(updateTime, 1000);
