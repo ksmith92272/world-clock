881dc9199b4cb1b1f6dcbdcba4ba3e4587666400
ksmith92272 <152031348+ksmith92272@users.noreply.github.com>
2024-01-10 18:00:51 -0600

    Set interval to update time every second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-let cities = document.querySelectorAll(".city");
+function updateTime() {
+	let cities = document.querySelectorAll(".city");
 
-cities.forEach(function (element) {
-	let dateElement = element.querySelector(".date");
-	let timeElement = element.querySelector(".time");
-	let timezone = element.getAttribute("id");
-	let current = moment().tz(timezone);
+	cities.forEach(function (element) {
+		let dateElement = element.querySelector(".date");
+		let timeElement = element.querySelector(".time");
+		let timezone = element.getAttribute("id");
+		let current = moment().tz(timezone);
 
-	dateElement.innerHTML = `${current.format("MMMM Do, YYYY")}`;
-	timeElement.innerHTML = `${current.format(
-		"hh:mm:ss"
-	)} <small>${current.format("A")}</small>`;
-});
+		dateElement.innerHTML = current.format("MMMM Do, YYYY");
+		timeElement.innerHTML = current.format("h:mm:ss [<small>]A[</small>]");
+	});
+}
+
+updateTime();
+setInterval(updateTime, 1000);
